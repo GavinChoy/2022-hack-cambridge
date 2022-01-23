e44800ed744bc4e8afb50762f732131ea5475604
ji21 <jeffreyip54@gmail.com>
2022-01-23 10:45:07 +0000

    .

--- frontend/dashboard/src/components/PatientsCard.jsx
+++ frontend/dashboard/src/components/PatientsCard.jsx
@@ -20,7 +20,30 @@ export default class PatientsCard extends Component {
         <div className="p-name">{this.props.name}</div>
       </div>
       <div className="p-main">
-
+        {/* <div>
+          New Message >>
+        </div> */}
+        <div className="pcard-text">
+        New Message >> {this.props.text.length > 0 ? this.props.text : "---"}
+        </div>
+        <div className="pcard-m">
+          <div>
+            <div>Possible Problems</div>
+            <ul>
+              {this.props.problems.map(p=>{
+                <li>p</li>
+              })}
+            </ul>
+          </div>
+          <div className="actions">
+            <div>Suggested Actions</div>
+            <ul>
+              {this.props.actions.map(a=>{
+                <li>a</li>
+              })}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
     );
