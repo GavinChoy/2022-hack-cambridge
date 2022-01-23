4f9244f3a4ab889cfa4a502105f56f8c39170554
rmdocherty <rmdocherty2@gmail.com>
2022-01-23 11:48:02 +0000

    Maps and image

--- frontend/dashboard/src/components/Map.jsx
+++ frontend/dashboard/src/components/Map.jsx
@@ -13,27 +13,44 @@ export default class Map extends Component {
     canvas.width = img.width
     canvas.height = img.height
     ctx.drawImage(img, 0, 0)
-    this.drawPatientDots()
+    //this.drawPatientDots()
+    window.requestAnimationFrame(this.drawPatientDots)
+  }
+
+  resetCanvas = () => {
+    let img = document.getElementById("ward")
+    let canvas = document.getElementById("canvas")
+    let ctx = canvas.getContext('2d')
+    console.log(img)
+    canvas.width = img.width
+    canvas.height = img.height
+    ctx.drawImage(img, 0, 0)
   }
 
   drawPatientDots = () => {
     let canvas = document.getElementById("canvas")
     let ctx = canvas.getContext('2d')
-      
+    this.resetCanvas()
+    let t = (Date.now() % 30) / 10
     for (let coord of this.patient_locs){
-      console.log(coord[0])
+      console.log(t)
       ctx.fillStyle = "#05ff1a"
       ctx.beginPath()
-      ctx.arc(coord[0], coord[1], 20, 0, 2 * Math.PI);
+      ctx.arc(coord[0], coord[1], 20 + t, 0, 2 * Math.PI);
       ctx.fill()
     }
+    window.requestAnimationFrame(this.drawPatientDots)
+  }
+
+  gotoMap = () => {
+    this.props.history.push('/patients');
   }
 
   render() {
     return (
     <div style={{"paddingLeft": "4%"}}>
-      <img id="ward" src={ward} style={{"display": "none"}} onLoad={this.onLoadImg}></img>
-      <canvas id="canvas" ></canvas>
+      <img id="ward" src={ward} style={{"display": "none"}} onLoad={this.onLoadImg} ></img>
+      <canvas id="canvas" onClick={this.gotoMap}></canvas>
     </div>);
   }
 }
